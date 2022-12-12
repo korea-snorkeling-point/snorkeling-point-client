abac70d41aeb9dce97c59d70fcc916633a1a9db1
hyejineee <dev.hyejin04@gmail.com>
2022-12-13 08:47:28 +0900

    chore : color값 정의 파일 추가

---
+++ src/commons/ts/colors.ts
@@ -0,0 +1,12 @@
+// 프로젝트에서 사용하는 color값을 정의하는 파일
+
+export const subYellow = "#FFD354";
+export const red = "#ED3F3F";
+export const mainBlue = "#6073FF";
+
+export const gradientStart = "#63EDED";
+export const gradientEnd = "#546FFF";
+
+export const gray8F = "#8F8F8F";
+export const grayF6 = "#F6F6F6";
+export const gray54 = "#545454 ";
