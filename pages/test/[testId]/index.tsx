adff6f5ee7bca67e3abba3f16fb85dad534549c4
hyejineee <dev.hyejin04@gmail.com>
2022-12-24 15:41:08 +0900

    chore : ec2 docker 이미지 pull&run 스크립트 작성성

--- pages/test/[testId]/index.tsx
+++ pages/test/[testId]/index.tsx
@@ -2,5 +2,5 @@ import { useRouter } from "next/router";
 
 export default function TestPage() {
   const router = useRouter();
-  return <div>{router.query.testId} 입니다.</div>;
+  return <div>테스트 번호{router.query.testId} 입니다.</div>;
 }
