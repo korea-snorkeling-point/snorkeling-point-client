c1989bfe16532e32664fdc1ecb7a10bc4405b14e
hyejineee <dev.hyejin04@gmail.com>
2022-12-24 16:10:29 +0900

    chore : 동적 배포 자동화 테스트

--- pages/test/[testId]/index.tsx
+++ pages/test/[testId]/index.tsx
@@ -2,5 +2,5 @@ import { useRouter } from "next/router";
 
 export default function TestPage() {
   const router = useRouter();
-  return <div>테스트 번호{router.query.testId} 입니다.</div>;
+  return <div> 동적 배포 자동화 테스트 번호{router.query.testId} 입니다.</div>;
 }
