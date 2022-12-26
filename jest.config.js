9e39d36f97c1dbcaeaf19eb615ca1109b253e9b4
hyejineee <dev.hyejin04@gmail.com>
2022-12-26 11:56:46 +0900

    chore : jest config 파일 수정

--- jest.config.js
+++ jest.config.js
@@ -7,15 +7,8 @@ const createJestConfig = nextJest({
 
 /** @type {import('jest').Config} */
 const customJestConfig = {
-  // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
-  setupFilesAfterEnv: [
-    '@testing-library/jest-dom/extend-expect',
-    '@testing-library/react',
-  ],
   collectCoverageFrom: [
     'src/**/*.[jt]s?(x)',
     'pages/**/*.[jt]s?(x)',
@@ -30,8 +23,6 @@ const customJestConfig = {
       lines: 90,
     },
   },
-
 };
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(customJestConfig);
