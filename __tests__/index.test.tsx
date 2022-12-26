28ad8dc0edfe93c512f63de18e7b3eb3edfcc5c4
hyejineee <dev.hyejin04@gmail.com>
2022-12-26 09:53:34 +0900

    settings : 테스트 환경 설정

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import TestPage from '../pages/index';
+
+describe('TestPage', () => {
+  const renderTestPage = () => render(<TestPage />);
+
+  it('renders a heading', () => {
+    const { container } = renderTestPage();
+    expect(container).toHaveTextContent('테스트 페이지');
+  });
+});
