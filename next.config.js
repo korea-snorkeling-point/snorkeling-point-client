a078d45584cf1e3a9b2375a119b7cc2963fc6116
hyejineee <dev.hyejin04@gmail.com>
2022-12-26 22:31:28 +0900

    settings : 린트 룰 추가 및 next config 웹팩 svg 관련 설정

--- next.config.js
+++ next.config.js
@@ -3,11 +3,18 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: false,
   trailingSlash: true,
-  generateBuildId: () => "KSNKPoint",
+  generateBuildId: () => 'KSNKPoint',
   exportPathMap: () => ({
-    "/": { page: "/" },
-    "/404": { page: "/404" },
+    '/': { page: '/' },
+    '/404': { page: '/404' },
   }),
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
