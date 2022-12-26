537f8b3c18a8ba4be08b146dfd0fa4414b28ca5e
hyejineee <dev.hyejin04@gmail.com>
2022-12-26 09:28:34 +0900

    settings : eslint 및 prettier 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import { Global } from "@emotion/react";
-import type { AppProps } from "next/app";
-import { resetStyles } from "../src/commons/ts/reset";
+import { Global } from '@emotion/react';
+import type { AppProps } from 'next/app';
+import { resetStyles } from '../src/commons/ts/reset';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
