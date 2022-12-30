eb23f152044b99340814f1ee4aa20caf9c9d52ac
hyejineee <dev.hyejin04@gmail.com>
2022-12-31 05:43:59 +0900

    test : useWindowSize 커스텀 훅 테스트 코드 작성

---
+++ __tests__/hooks/useWindowSize.test.ts
@@ -0,0 +1,57 @@
+import { useWindowSize } from '@hooks/useWindowSize';
+import { act, fireEvent } from '@testing-library/react';
+import { renderHook } from '@testing-library/react-hooks';
+
+describe('useWindowSize', () => {
+  const fakeHeight = 200;
+  const fakeWidth = 200;
+
+  beforeEach(() => {
+    window.innerHeight = fakeHeight;
+    window.innerWidth = fakeWidth;
+  });
+
+  it('현재 윈도우의 사이즈를 반환한다.', () => {
+    const { result } = renderHook(() => useWindowSize());
+
+    const actual = result.current[0];
+
+    expect(JSON.stringify(actual)).toBe(
+      JSON.stringify({
+        width: fakeWidth,
+        height: fakeHeight,
+      }),
+    );
+  });
+
+  context('윈도우 사이즈가 변경되면', () => {
+    const changeHeight = 100;
+    const changeWidth = 100;
+
+    it('변경된 사이즈를 반환한다.', async () => {
+      jest.useFakeTimers();
+      const { result } = renderHook(() => useWindowSize());
+
+      expect(JSON.stringify(result.current[0])).toBe(
+        JSON.stringify({
+          width: fakeWidth,
+          height: fakeHeight,
+        }),
+      );
+
+      act(() => {
+        window.innerHeight = changeHeight;
+        window.innerWidth = changeWidth;
+        fireEvent.resize(window);
+        jest.runAllTimers();
+      });
+
+      expect(JSON.stringify(result.current[0])).toBe(
+        JSON.stringify({
+          width: changeWidth,
+          height: changeHeight,
+        }),
+      );
+    });
+  });
+});
