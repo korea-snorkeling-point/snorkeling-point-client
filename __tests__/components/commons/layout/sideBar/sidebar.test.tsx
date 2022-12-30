3e92f69f160a827c18b08dde7e7f46e541269f5e
hyejineee <dev.hyejin04@gmail.com>
2022-12-31 05:45:08 +0900

    test : SideBar 컴포넌트 테스트 작성

--- __tests__/components/commons/layout/sideBar/sidebar.test.tsx
+++ __tests__/components/commons/layout/sideBar/sidebar.test.tsx
@@ -1,24 +1,66 @@
-import { fireEvent, render } from '@testing-library/react';
+import { act, fireEvent, render } from '@testing-library/react';
 import React from 'react';
 import SideBar from '@components/commons/layout/sideBar/sidebar';
-import { menus } from '@constants/menu';
+import menus from '@constants/menu';
+import { useRouter } from 'next/router';
+import { matchers } from '@emotion/jest';
+import { prettyDOM } from '@testing-library/dom';
+
+expect.extend(matchers);
+
+jest.mock('next/router');
 
 describe('SideBarContainer', () => {
+  (useRouter as jest.Mock).mockImplementation(() => ({
+    asPath: '',
+  }));
+
   const renderSideBar = () => render(<SideBar />);
 
-  it('메뉴를 보여준다.', () => {
+  it('메뉴 타이틀을 보여준다.', () => {
     const { container } = renderSideBar();
-
-    menus.forEach(({ label }) => {
-      expect(container).toHaveTextContent(label);
+    menus.forEach(({ title }) => {
+      expect(container).toHaveTextContent(title);
     });
   });
 
   context('메뉴를 클릭하면', () => {
-    it('상태변경 메소드가 호출된다.', () => {
-      const { container, getByText } = renderSideBar();
+    (useRouter as jest.Mock).mockImplementation(() => ({
+      asPath: '/main',
+      push: jest.fn(),
+    }));
+    const router = useRouter();
 
-      // fireEvent.click(getByText('홈'));
+    it('메뉴에 해당하는 페이지 path를 라우터에 설정한다.', () => {
+      const { getByText } = renderSideBar();
+
+      fireEvent.click(getByText('홈'));
+
+      expect(router.asPath).toBe('/main');
+    });
+  });
+
+  context('메뉴 토글 버튼을 클릭하면', () => {
+    it('메뉴 타이틀이 보인다.', () => {
+      const { container, getByAltText, getAllByRole } = renderSideBar();
+
+      const menuToggleButton = getByAltText('menuToggleIcon');
+
+      expect(menuToggleButton).not.toBeNull();
+      const menus = getAllByRole('listitem', { name: '' });
+
+      expect(menus[0]).toHaveStyleRule('visibility', 'hidden', {
+        media: '@media (max-width: 575px)',
+      });
+
+      act(() => {
+        fireEvent.click(menuToggleButton);
+      });
+
+      expect(container).toHaveTextContent('홈');
+      expect(menus[0]).toHaveStyleRule('visibility', 'visible', {
+        media: '@media (max-width: 575px)',
+      });
     });
   });
 });
