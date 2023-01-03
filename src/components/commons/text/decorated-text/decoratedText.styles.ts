6dca11b7eb72770d5e018bd843f3db66ebc027fb
kyeongwxx <kyeongwxx@gmail.com>
2023-01-03 23:25:54 +0900

    fix: DecoratedText 컴포넌트 underline z-index 추가

--- src/components/commons/text/decorated-text/decoratedText.styles.ts
+++ src/components/commons/text/decorated-text/decoratedText.styles.ts
@@ -12,46 +12,42 @@ export const DecoratedText = styled.div(
     p::before {
       content: '';
       position: absolute;
-      bottom: 3px;
+      bottom: 1px;
       width: 100%;
       left: 0;
       height: 8px;
       border-radius: 2px;
       background: linear-gradient(273.41deg, rgba(84, 111, 255, 0.14) -22.5%, #63eded 100%);
       opacity: 0.8;
+      z-index: -1;
     }
 
     ${R.setDeskTopStyle(css`
-      font-size: 
-        ${props.style.fontSize === 'sp' ? `${S.DeskTopFontSizeSupper}rem` : ''}
+      font-size: ${props.style.fontSize === 'sp' ? `${S.DeskTopFontSizeSupper}rem` : ''}
         ${props.style.fontSize === 'lg' ? `${S.DeskTopFontSizeLg}rem` : ''}
         ${props.style.fontSize === 'md' ? `${S.DeskTopFontSizeMiddle}rem` : ''};
     `)}
 
     ${R.setNoteBookStyle(css`
-      font-size: 
-        ${props.style.fontSize === 'sp' ? `${S.NoteBookFontSizeSupper}rem` : ''}
+      font-size: ${props.style.fontSize === 'sp' ? `${S.NoteBookFontSizeSupper}rem` : ''}
         ${props.style.fontSize === 'lg' ? `${S.NoteBookFontSizeLg}rem` : ''}
         ${props.style.fontSize === 'md' ? `${S.NoteBookFontSizeMiddle}rem` : ''};
     `)}
 
     ${R.setBigTabletStyle(css`
-      font-size: 
-        ${props.style.fontSize === 'sp' ? `${S.BigTabletFontSizeSupper}rem` : ''}
+      font-size: ${props.style.fontSize === 'sp' ? `${S.BigTabletFontSizeSupper}rem` : ''}
         ${props.style.fontSize === 'lg' ? `${S.BigTabletFontSizeLg}rem` : ''}
         ${props.style.fontSize === 'md' ? `${S.BigTabletFontSizeMiddle}rem` : ''};
     `)}
 
     ${R.setSmallTabletStyle(css`
-      font-size: 
-        ${props.style.fontSize === 'sp' ? `${S.SmallTabletFontSizeSupper}rem` : ''}
+      font-size: ${props.style.fontSize === 'sp' ? `${S.SmallTabletFontSizeSupper}rem` : ''}
         ${props.style.fontSize === 'lg' ? `${S.SmallTabletFontSizeLg}rem` : ''}
         ${props.style.fontSize === 'md' ? `${S.SmallTabletFontSizeMiddle}rem` : ''};
     `)}
 
     ${R.setMobileStyle(css`
-      font-size: 
-        ${props.style.fontSize === 'sp' ? `${S.MobileFontSizeSupper}rem` : ''}
+      font-size: ${props.style.fontSize === 'sp' ? `${S.MobileFontSizeSupper}rem` : ''}
         ${props.style.fontSize === 'lg' ? `${S.MobileFontSizeLg}rem` : ''}
         ${props.style.fontSize === 'md' ? `${S.MobileFontSizeMiddle}rem` : ''};
     `)}
