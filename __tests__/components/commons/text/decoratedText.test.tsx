d9af318b45906ca6cdb6d28084a1514777079d44
kyeongwxx <kyeongwxx@gmail.com>
2023-01-05 00:31:38 +0900

    test: DecoratedText 컴포넌트 size props 테스트 추가

--- __tests__/components/commons/text/decoratedText.test.tsx
+++ __tests__/components/commons/text/decoratedText.test.tsx
@@ -1,24 +1,21 @@
 import DecoratedText from '@components/commons/text/decorated-text/decoratedText';
-import { render, screen } from '@testing-library/react';
+import { FONT_SIZE } from '@constants/decoratedText';
+import { render } from '@testing-library/react';
 
 describe('<DecoratedText /> 컴포넌트', () => {
-  context('렌더링 시', () => {
-    it('컴포넌트를 정상 렌더링한다.', () => {
-      render(<DecoratedText value="rendering test" size="sp" />);
-    });
-  });
-
-  context('screen을 불러들이면', () => {
-    it('HTML 태그와 내용을 출력한다.', () => {
-      render(<DecoratedText value="value props debugging" size="sp" />);
-      screen.debug();
-    });
-  });
+  const component = () =>
+    render(<DecoratedText value="value props test" size={FONT_SIZE.SUPPER} />);
+  const { container } = component();
 
   context('value props를 함께 전달하면', () => {
     it('value props를 렌더링한다.', () => {
-      render(<DecoratedText value="value props test" size="sp" />);
-      expect(screen.getByText('value props test')).toBeInTheDocument();
+      expect(container).toHaveTextContent('value props test');
+    });
+  });
+
+  context('size props를 함께 전달하면', () => {
+    it('전달한 size props에 따른 font-size를 갖는다.', () => {
+      expect(container).toHaveStyle(`font-size:${FONT_SIZE.SUPPER};`);
     });
   });
 });
