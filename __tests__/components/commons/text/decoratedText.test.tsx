aed8e7087d383e10c9b79ca47f3e802580030a26
kyeongwxx <kyeongwxx@gmail.com>
2023-01-05 01:28:49 +0900

    test: DecoratedText 컴포넌트 각 테스트마다 렌더링하도록 수정(독립성 유지)

--- __tests__/components/commons/text/decoratedText.test.tsx
+++ __tests__/components/commons/text/decoratedText.test.tsx
@@ -3,19 +3,47 @@ import { FONT_SIZE } from '@constants/decoratedText';
 import { render } from '@testing-library/react';
 
 describe('<DecoratedText /> 컴포넌트', () => {
-  const component = () =>
-    render(<DecoratedText value="value props test" size={FONT_SIZE.SUPPER} />);
-  const { container } = component();
-
   context('value props를 함께 전달하면', () => {
     it('value props를 렌더링한다.', () => {
+      const componentSupper = () =>
+        render(
+          <DecoratedText value="value props test" size={FONT_SIZE.SUPPER} />,
+        );
+      const { container } = componentSupper();
       expect(container).toHaveTextContent('value props test');
     });
   });
 
   context('size props를 함께 전달하면', () => {
     it('전달한 size props에 따른 font-size를 갖는다.', () => {
+      const componentSupper = () =>
+        render(
+          <DecoratedText value="value props test" size={FONT_SIZE.SUPPER} />,
+        );
+      const { container } = componentSupper();
       expect(container).toHaveStyle(`font-size:${FONT_SIZE.SUPPER};`);
+    });
+  });
+
+  context('size props를 함께 전달하면', () => {
+    it('전달한 size props에 따른 font-size를 갖는다.', () => {
+      const componentLarge = () =>
+        render(
+          <DecoratedText value="value props test" size={FONT_SIZE.LARGE} />,
+        );
+      const { container } = componentLarge();
+      expect(container).toHaveStyle(`font-size:${FONT_SIZE.LARGE};`);
+    });
+  });
+
+  context('size props를 함께 전달하면', () => {
+    it('전달한 size props에 따른 font-size를 갖는다.', () => {
+      const componentMiddle = () =>
+        render(
+          <DecoratedText value="value props test" size={FONT_SIZE.MIDDLE} />,
+        );
+      const { container } = componentMiddle();
+      expect(container).toHaveStyle(`font-size:${FONT_SIZE.MIDDLE};`);
     });
   });
 });
