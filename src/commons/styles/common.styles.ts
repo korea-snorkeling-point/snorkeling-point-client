9c8d9b963457580ebc4a6087a55046675dca252f
hyejineee <dev.hyejin04@gmail.com>
2023-01-30 15:58:46 +0900

    feat : ChipText 공통 스타일 컴포넌트 생성

--- src/commons/styles/common.styles.ts
+++ src/commons/styles/common.styles.ts
@@ -2,9 +2,21 @@ import styled from '@emotion/styled';
 import * as C from '@styles/colors.styles';
 import * as R from '@styles/responsive.styles';
 import * as S from '@styles/dimen.styles';
+import { css } from '@emotion/react';
+import { IStyleProps } from './types';
 
 export const ErrorText = styled.p`
   color: ${C.red};
   ${R.dynamicSize({ attribute: 'marginTop', px: 6 })}
   ${S.setFontSizePico()}
 `;
+
+export const ChipText = styled.span(
+  (props: IStyleProps) => css`
+    background-color: ${props.backgroundColor || 'white'};
+    color: ${props.color || 'black'};
+    padding: 5px 12px;
+    border-radius: 50px;
+    ${S.setFontSizePico()}
+  `,
+);
