98ac964dc36fb56ca30e397199ebe2f94cc4b41f
hyejineee <dev.hyejin04@gmail.com>
2023-01-30 18:17:45 +0900

    feat : SnorkelingPointCard 컴포넌트 생성

---
+++ src/components/commons/Card/normalCard/snorkelingPoincard/snorkelingPointCard.styles.tsx
@@ -0,0 +1,72 @@
+import styled from '@emotion/styled';
+import * as R from '@styles/responsive.styles';
+import * as S from '@styles/dimen.styles';
+import { IStyleProps } from '@styles/types';
+import { css } from '@emotion/react';
+
+export const Wrapper = styled.div`
+  width: 100%;
+  background-color: white;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+
+  ${R.dynamicSize({ attribute: 'padding', px: 24 })}
+  ${R.dynamicSize({ attribute: 'marginBottom', px: 16 })}
+`;
+
+export const HeaderWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  ${R.dynamicSize({ attribute: 'marginBottom', px: 30 })}
+`;
+
+export const TitleWrapper = styled.h2`
+  font-weight: 700;
+  ${S.setFontSizeMiddle()}
+`;
+
+export const LikeWrapper = styled.div`
+  ${R.dynamicSize({ attribute: 'width', px: 24 })}
+`;
+
+export const ImageWrapper = styled.img(
+  (props: IStyleProps) => css`
+    width: ${props.width || '100%'};
+    margin-right: 8px;
+    border-radius: 10px;
+    ${R.dynamicSize({ attribute: 'marginBottom', px: props.marginBottom || 0 })}
+  `,
+);
+
+export const DescriptionWrapper = styled.p`
+  color: #292d32;
+  overflow: hidden;
+  word-break: break-all;
+  word-wrap: break-word;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  ${S.setFontSizeMicro()}
+  ${R.dynamicSize({ attribute: 'lineHeight', px: 18 })}
+  ${R.dynamicSize({ attribute: 'marginBottom', px: 20 })}
+`;
+
+export const AddressWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  p {
+    color: #292d32;
+    ${S.setFontSizeMicro()}
+  }
+
+  ${R.dynamicSize({ attribute: 'marginBottom', px: 20 })}
+`;
+
+export const TagsWrppaer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px;
+`;
---
+++ src/components/commons/Card/normalCard/snorkelingPoincard/snorkelingPointCard.tsx
@@ -0,0 +1,48 @@
+import ToggleIcon from '@components/commons/toggle/toggleIcon/toggleIcon';
+import { ChipText } from '@styles/common.styles';
+import * as C from '@styles/colors.styles';
+import * as S from './snorkelingPointCard.styles';
+
+export default function SnorkelingPointCard() {
+  return (
+    <S.Wrapper>
+      <S.HeaderWrapper>
+        <S.TitleWrapper>삼척 장호항</S.TitleWrapper>
+        <S.LikeWrapper>
+          <ToggleIcon
+            isOn
+            onIcon={<img src="/icon/ic_like_fill.svg" alt="like-icon" />}
+            offIcon={<img src="/icon/ic_like_fill.svg" alt="unlike-icon" />}
+          />
+        </S.LikeWrapper>
+      </S.HeaderWrapper>
+
+      <S.ImageWrapper
+        src="https://images.unsplash.com/photo-1518467166778-b88f373ffec7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2662&q=80"
+        marginBottom={16}
+      />
+
+      <S.DescriptionWrapper>
+        남는 목숨을 곳이 그들은 생명을 그림자는 우리 봄바람이다. 그림자는 그와
+        속에 귀는 구하지 끝까지 없으면 평화스러운 아름다우냐? 없으면 있음으로써
+        피에 기관과 두손을 얼마나 이것이다. 대한 꽃이 그들을 기쁘며, 능히 않는
+        뿐이다. 우리 불어 가치를 이상은 소리다.이것은 청춘의 철환하였는가?
+      </S.DescriptionWrapper>
+
+      <S.AddressWrapper>
+        <S.ImageWrapper src="/icon/ic_location_blue.svg" width="24px" />
+        <p>강원 삼척시 근덕면 장호리</p>
+      </S.AddressWrapper>
+
+      <S.TagsWrppaer>
+        {Array(5)
+          .fill('tag')
+          .map(e => (
+            <ChipText backgroundColor={C.subYellow} color="white">
+              {e}
+            </ChipText>
+          ))}
+      </S.TagsWrppaer>
+    </S.Wrapper>
+  );
+}
