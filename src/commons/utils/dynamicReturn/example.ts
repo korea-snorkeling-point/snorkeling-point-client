d902e056b43f4082f013fc07be2d1385c39b7d51
hyejineee <dev.hyejin04@gmail.com>
2023-02-14 17:55:41 +0900

    refactor : 클래스명 변경, 생성자 디폴트 값 설정

--- src/commons/utils/dynamicReturn/example.ts
+++ src/commons/utils/dynamicReturn/example.ts
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client';
 import { IUser } from 'src/commons/graphql-types/generated/types';
 import { IProduct } from 'src/commons/graphqlTypes/generated/types';
 import Field from './Field';
-import ObjectType from './ObjectType';
+import ObjectField from './ObjectField';
 
 const CREATE_USER = (returnData: string) => gql`
   mutation createUser($createUserInput : CreateUserInput!){
@@ -12,11 +12,11 @@ const CREATE_USER = (returnData: string) => gql`
   }
 `;
 
-const returnValues = new ObjectType<IUser | IProduct>('')
+const returnValues = new ObjectField<IUser | IProduct>()
   .add(new Field<IUser>('email'))
   .add(new Field<IUser>('nickname'))
   .add(new Field<IUser>('profileImg'))
-  .add(new ObjectType<IProduct>('product').add(new Field<IProduct>('title')))
+  .add(new ObjectField<IProduct>('product').add(new Field<IProduct>('title')))
   .toQuery();
 
 const query = CREATE_USER(returnValues);
