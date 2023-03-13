e0091cd6e51bec49689a9e335754a9abb691590c
hyejineee <dev.hyejin04@gmail.com>
2023-03-14 02:10:22 +0900

    test : useCreateUser 커스텀 훅 테스트 작성

--- src/commons/constants/errorMessage.ts
+++ src/commons/constants/errorMessage.ts
@@ -3,6 +3,7 @@ const AUTH = {
   EMAIL_REQUIRE: '이메일을 입력해 주세요.',
   PASSWORD: '영문, 숫자 조합 8~16 자리의 비밀번호를 입력해 주세요.',
   PASSWORD_CHECK: '비밀번호가 일치하지 않습니다.',
+  PASSWORD_REQUIRE: '비밀번호를 입력해 주세요',
   NICKNAME_REQUIRE: '닉네임을 입력해 주세요.',
   GENDER_REQUIRE: '성별을 입력해 주세요.',
   VERIFY_CODE_REQUIRE: '인증번호를 입력해 주세요.',
